bf2fad5151a0b220a0cc83019c57d722254b0373
vishwasdev01 <vishwasdev01@gmail.com>
2025-03-07 12:49:06 +0530

    change in axios

--- axiosInstance.jsx
+++ axiosInstance.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 let ENV_NODE = "LOCAL";
   const url = window.location.href;
-  if (url.includes("aibot.elementtechnologies.net")) {
+  if (url.includes("personal-finance-tracker-bac-git-41767a-vishwas-nahars-projects.vercel.app")) {
     ENV_NODE="PROD"
   } else if (url.includes("localhost")) {
     ENV_NODE="LOCAL"
   }
 const BASE_URL_NODE = {
   LOCAL: "http://localhost:5000",
-  PROD: "https://aibogies.net",
+  PROD: "https://personal-finance-tracker-bac-git-41767a-vishwas-nahars-projects.vercel.app/",
 };
 
 export const axiosInstance = axios.create({
