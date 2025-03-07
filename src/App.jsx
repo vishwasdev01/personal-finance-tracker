5fa8c52531449259259da4afce5d82a202a93332
vishwasdev01 <vishwasdev01@gmail.com>
2025-03-07 16:24:05 +0530

    remove commented lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import AddTransaction from "./pages/AddTransaction";
-// import { Container } from "@mui/material";
-// import TransactionDetails from "./pages/TransactionDetails";
-// import TransactionSummary from "./pages/TransactionalSummary";
-
-// const App = () => (
-//   <Router>
-//     <Container style={{position:"fixed", top:5}}>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/create-transaction" element={<AddTransaction />} />
-//         <Route path="/transaction/:id" element={<TransactionDetails />} />
-//         <Route path="/edit-transaction/:id" element={<AddTransaction isEdit={true} />} />
-//         <Route path="/transaction/summary" element={<TransactionSummary />} />
-
-//       </Routes>
-//     </Container>
-//   </Router>
-// );
-
-// export default App; 
 
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
